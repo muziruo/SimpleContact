2fa6377a12d346e50bb7d2286341983ec98dce43
muziruo <1149354821@qq.com>
2018-06-13 20:24:43 +0800

    Initial Commit

---
+++ SimpleContact/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleContact
+//
+//  Created by 李祎喆 on 2018/6/13.
+//  Copyright © 2018年 李祎喆. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
